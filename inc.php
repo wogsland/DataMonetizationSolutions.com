0fa705bb3d6604cd5d91c79bd1100b1ac8df9eae
Bradley Wogsland <bradley@wogsland.org>
2015-09-14 11:13:02 -0500

    testing form

--- inc.php
+++ inc.php
@@ -1,6 +1,9 @@
 <?php
+echo 'before funcs<br>';
 include_once 'functions.php';
+echo 'before creds<br>';
 include_once 'credentials.php';
 
+echo 'before mysqli<br>';
 $mysqli = new mysqli($host, $user, $pass, $database);
 ?>
