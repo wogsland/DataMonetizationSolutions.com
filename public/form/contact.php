9c3d1efa5e7efb98687eb1a4b778e70d68ab8e54
Bradley Wogsland <bradley@wogsland.org>
2015-09-14 11:11:35 -0500

    testing form

--- public/form/contact.php
+++ public/form/contact.php
@@ -1,13 +1,16 @@
 <?php
+echo 'before req<br>';
 require_once '../inc.php';
+echo 'after req<br>';
+print_r($_POST);
 
 $vars = array('name', 'email', 'comments');
 foreach ($vars as $var) {
-  $$var = isset($_POST[$var]) ? $mysqli->real_escape_string($_POST[$var]) : '';
+    $$var = isset($_POST[$var]) ? $mysqli->real_escape_string($_POST[$var]) : '';
 }
 
 $query = "INSERT INTO contact_form_responses (name, email, comments)
-          VALUES ('$name', '$email', '$comments')"
+          VALUES ('$name', '$email', '$comments')";
 $mysqli->query($query);
 
 redirect('/thank-you.html');
