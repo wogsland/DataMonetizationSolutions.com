10c206ed7162916f251da10357952fbf25680896
Bradley Wogsland <bradley@wogsland.org>
2015-09-24 15:54:57 -0500

    footer

--- public/404.php
+++ public/404.php
@@ -7,5 +7,13 @@ require __DIR__.'/../include/head.php';
 <?php
 require __DIR__.'/../include/nav.php';
 ?>
-
-That's a 404 error right there. Yup.
+<div class="container about">
+    <div class="row">
+        <div class="col-lg-12">
+            That's a 404 error right there. Yup.
+        </div>
+    </div>
+</div>
+<?php
+require __DIR__.'/../include/footer.php';
+?>
