55b103b16b24f0ba911b146703ed34926bdf5a9a
Bradley Wogsland <bradley@wogsland.org>
2015-09-24 15:58:48 -0500

    footer

--- public/about.php
+++ public/about.php
@@ -31,27 +31,6 @@ require __DIR__.'/../include/nav.php';
     </div><!-- /.container -->
 
 
-    <div class="container">
-      <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>
-          &copy; 2015 Data Monetization Solutions &middot;
-          <a href="privacy.html">Privacy</a>
-          &middot; <a href="terms.html">Terms</a>
-        </p>
-      </footer>
-    </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+    <?php
+    require __DIR__.'/../include/footer.php';
+    ?>
--- public/index.php
+++ public/index.php
@@ -132,29 +132,6 @@ require __DIR__.'/../include/nav.php';
       <!-- /END THE FEATURETTES -->
 
 
-      <!-- FOOTER -->
-      <footer>
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>
-          &copy; 2015 Data Monetization Solutions &middot;
-          <a href="privacy.html">Privacy</a>
-          &middot; <a href="terms.html">Terms</a>
-        </p>
-      </footer>
-
-    </div><!-- /.container -->
-
-    <div class="white-style ">
-      <a href="http://www.freepik.com/free-photos-vectors/education">Education vector designed by Freepik</a>
-    </div>
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
-    <script src="js/vendor/holder.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
-  </body>
-</html>
+      <?php
+      require __DIR__.'/../include/footer.php';
+      ?>
